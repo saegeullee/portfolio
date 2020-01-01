70ca6563180f610ffe2cc3b8a0c82d32bb6510f5
saegeullee <lemix777@naver.com>
2020-01-01 21:20:31 +0900

    Updates

---
+++ precache-manifest.5828cc38af19d0d27e4d6a4d95b5c0f0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ccf8d86c29bfeb0a164459fad028b0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bf10f31405c4c49e5f03",
+    "url": "/portfolio/static/css/main.a618be8f.chunk.css"
+  },
+  {
+    "revision": "492cd0eb41ac9e493eae",
+    "url": "/portfolio/static/js/2.d95a2ecd.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/portfolio/static/js/2.d95a2ecd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf10f31405c4c49e5f03",
+    "url": "/portfolio/static/js/main.a4d03cbd.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "e9e703369370aa15511eab84aa24edc8",
+    "url": "/portfolio/static/media/class101_creator_1.e9e70336.gif"
+  },
+  {
+    "revision": "0f8fe14a468e0a4d9bb8777e1568c28b",
+    "url": "/portfolio/static/media/class101_creator_2.0f8fe14a.gif"
+  },
+  {
+    "revision": "634f692e128e4f91d914262e691d7c4f",
+    "url": "/portfolio/static/media/class101_user_1.634f692e.gif"
+  },
+  {
+    "revision": "b92939fb64471255b693815a42746754",
+    "url": "/portfolio/static/media/ex_admin_gif_1.b92939fb.gif"
+  },
+  {
+    "revision": "4d793a022f8af0fc9f25cf0aa6f454ec",
+    "url": "/portfolio/static/media/ex_admin_gif_2.4d793a02.gif"
+  },
+  {
+    "revision": "8a74234ae08c31ad1ca07d87a53dc1ab",
+    "url": "/portfolio/static/media/ex_admin_gif_3.8a74234a.gif"
+  },
+  {
+    "revision": "9b36eb15562ea15977d51fa6abbf2ade",
+    "url": "/portfolio/static/media/ex_admin_gif_4.9b36eb15.gif"
+  },
+  {
+    "revision": "be8e77c41a818e14a6b19cd480499c53",
+    "url": "/portfolio/static/media/westagram_gif_1.be8e77c4.gif"
+  },
+  {
+    "revision": "471e713da61b046f1ae9041594463215",
+    "url": "/portfolio/static/media/westagram_gif_2.471e713d.gif"
+  },
+  {
+    "revision": "8f1a85b6783183416e0d3b96795ab519",
+    "url": "/portfolio/static/media/westagram_gif_3.8f1a85b6.gif"
+  },
+  {
+    "revision": "acf72bc5305c6b4c9a270d762d074ef2",
+    "url": "/portfolio/static/media/yogiyong_1.acf72bc5.gif"
+  },
+  {
+    "revision": "d9cdf3b8c51092a9048ab5f75b3f446d",
+    "url": "/portfolio/static/media/yogiyong_2.d9cdf3b8.gif"
+  },
+  {
+    "revision": "3834aec76855856d3d72ac59148b8672",
+    "url": "/portfolio/static/media/yogiyong_3.3834aec7.gif"
+  }
+]);
