18c64ecf0f8e9ad8672c093cf7bc9e488ca67cd4
saegeullee <lemix777@naver.com>
2020-01-09 02:51:04 +0900

    updated data and font family

--- src/data/data.js
+++ src/data/data.js
@@ -142,7 +142,7 @@ export const projectsData = [
 ];
 
 export const intro_title_1 = '안녕하세요 저는 이새글입니다.';
-export const intro_title_2 = '제품을 통해 사용자를 기쁘게 만들고 싶은 백엔드 개발자입니다.';
+export const intro_title_2 = '소프트웨어를 통해 사용자를 행복하게 만들고 싶은 백엔드 개발자입니다.';
 
 export const intro_details_1 =
   '새로운 기술에 관심이 많으며 동료들과의 커뮤니케이션과 상호 피드백 및 리뷰를 좋아합니다. 현재 신입 백엔드 개발자로 구직중입니다. 데이터에 기반하여 의사결정을 하는 회사에서 일하고 싶습니다.';
